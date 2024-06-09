766529d4218f11ed2fa4217afa30b865635aa980
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-09 11:20:15 +0900

    Update pi.py

--- models/pi.py
+++ models/pi.py
@@ -5,7 +5,7 @@ from models.transformer import TransformerEncoder
 class AdjacencyGenerator(nn.Module):
     def __init__(self, d_model, num_heads, d_ff, num_layers, hidden_size, device, dropout=0.1):
         super(AdjacencyGenerator, self).__init__()
-        self.encoder = TransformerEncoder(d_model, num_heads, d_ff, num_layers, dropout).to(device)
+        self.cross_attention = nn.MultiheadAttention(d_model, num_heads, dropout=dropout).to(device)
         self.device = device
 
         self.weight_layer = nn.Linear(d_model, 2*d_model).to(device)
@@ -13,28 +13,19 @@ class AdjacencyGenerator(nn.Module):
         self.weight_vector = nn.Linear(2*d_model, 1).to(device)
 
     def forward(self, node_features, neighbor_features):
-        input_features = torch.cat([node_features, neighbor_features], dim=0).unsqueeze(0)  # (1, num_neighbors + 1, d_model)
-        adj_logits = self.encoder(input_features.clone())  # (num_neighbors + 1, 1, d_model)        
+        # Queryはnode_features、KeyとValueはneighbor_features
+        query = neighbor_features.unsqueeze(1)  # (num_neighbors, 1, d_model)
+        key = node_features.unsqueeze(1)  # (1, num_nodes, d_model)
+        value = node_features.unsqueeze(1)  # (1, num_nodes, d_model)
 
-        # for layer in self.encoder.encoder.layers:
-        #     # print(f"layer: {layer}")
-        #     sa_output, _ = layer.self_attn(input_features, input_features, input_features, need_weights=False)
-        #     sa_output = layer.dropout1(sa_output)
-        #     sa_output = layer.norm1(input_features + sa_output).clone()
+        attn_output, attn_output_weights = self.cross_attention(query, key, value)  # クロスアテンションを適用
 
-        #     # Clone weights and biases for feed-forward network
-        #     ff_output = torch.nn.functional.linear(sa_output, layer.linear1.weight.clone(), layer.linear1.bias.clone())
-        #     ff_output = layer.activation(ff_output)
-        #     ff_output = layer.dropout(ff_output).clone()      
-        #     ff_output = torch.nn.functional.linear(ff_output, layer.linear2.weight.clone(), layer.linear2.bias.clone())
-        #     ff_output = layer.dropout2(ff_output).clone()
-        #     input_features = layer.norm2(sa_output + ff_output).clone()
-
-        adj_logits = nn.functional.linear(input_features, self.weight_layer.weight.clone(), self.weight_layer.bias)
+        adj_logits = attn_output.squeeze(0)  # (1, d_model) -> (d_model)
+        
+        adj_logits = nn.functional.linear(adj_logits, self.weight_layer.weight.clone(), self.weight_layer.bias)
         adj_logits = nn.functional.linear(adj_logits, self.weight_layer2.weight.clone(), self.weight_layer2.bias)
         adj_logits = nn.functional.linear(adj_logits, self.weight_vector.weight.clone(), self.weight_vector.bias).squeeze(1)
         adj_probs = torch.sigmoid(adj_logits / 3).to(self.device)  # Reduce to (num_neighbors + 1)
-
 
         return adj_probs, adj_logits
 
