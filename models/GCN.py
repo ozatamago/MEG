0509342747cf8a86503ccbd5f51c272e4556ca8b
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-10 16:54:27 +0900

    Update GCN.py

--- models/GCN.py
+++ models/GCN.py
@@ -9,15 +9,25 @@ class GCN(nn.Module):
         assert num_layers >= 2, "Number of layers must be at least 2"
 
         self.convs = nn.ModuleList()
+        self.bns = nn.ModuleList()
+
         self.convs.append(GCNConv(in_channels, hidden_channels))
+        self.bns.append(nn.BatchNorm1d(hidden_channels))
 
         for _ in range(num_layers - 2):
             self.convs.append(GCNConv(hidden_channels, hidden_channels))
+            self.bns.append(nn.BatchNorm1d(hidden_channels))
 
         self.convs.append(GCNConv(hidden_channels, out_channels))
 
     def forward(self, x, edge_index):
-        for conv in self.convs[:-1]:
-            x = F.relu(conv(x, edge_index), inplace=False)
-        x = self.convs[-1](x, edge_index)  # No ReLU on the last layer
+        for conv, bn in zip(self.convs[:-1], self.bns):
+            x = conv(x, edge_index)
+            x = bn(x)
+            x = F.relu(x, inplace=False)
+        x = self.convs[-1](x, edge_index)  # No ReLU or BatchNorm on the last layer
         return x
+
+# Example usage:
+# model = GCN(in_channels=16, hidden_channels=32, out_channels=2, num_layers=4)
+# out = model(data.x, data.edge_index)
