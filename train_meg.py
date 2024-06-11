91b7f2e9ba97792e51849f7eb7a9dbd2a8e5e5a0
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-11 17:45:12 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -306,7 +306,7 @@ def train(rank, world_size):
                 log_probs_with_adv = log_probs_layers[count] * advantages_layers[count]
                 # visualize_tensor(log_probs_with_adv, f"log_probs_with_adv_graph_{count}")
                 log_probs_with_adv.backward(retain_graph=True)
-                # torch.nn.utils.clip_grad_norm_(adj_generator.parameters(), max_norm=0.1)
+                torch.nn.utils.clip_grad_norm_(adj_generator.parameters(), max_norm=0.1)
                 opt_adj.step()  # 各 optimizer_adj に対してステップ
                 count = count + 1
 
