126b7089f65279a5cad74f4c5dbd54102abc0346
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-11 23:53:55 +0900

    Update GCN.py

--- models/GCN.py
+++ models/GCN.py
@@ -1,64 +1,32 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
-
-import torch
-from torch.nn.parameter import Parameter
-from torch.nn.modules.module import Module
-
-
-class GraphConvolution(Module):
-    """
-    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
-    """
-
-    def __init__(self, in_features, out_features, bias=True):
-        super(GraphConvolution, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
-
-    def forward(self, input, adj):
-        support = torch.mm(input, self.weight)
-        output = torch.spmm(adj, support)
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
+from torch_geometric.nn import GCNConv
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
         super(GCN, self).__init__()
+        assert num_layers >= 2, "Number of layers must be at least 2"
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
-        self.dropout = dropout
+        self.convs = nn.ModuleList()
+        self.relus = nn.ModuleList()
 
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, x))
-        x = F.dropout(x, self.dropout, training=self.training)
+        self.convs.append(GCNConv(in_channels, hidden_channels))
+        self.relus.append(nn.ReLU(inplace=False))
+
+        for _ in range(num_layers - 2):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels))
+            self.relus.append(nn.ReLU(inplace=False))
+
+        self.convs.append(GCNConv(hidden_channels, out_channels))
+
+    def forward(self, x, edge_index):
+        for conv, relu in zip(self.convs[:-1], self.relus):
+            x = conv(x, edge_index)
+            x = relu(x)
+        x = self.convs[-1](x, edge_index)  # No ReLU on the last layer
         return x
 
-class FinalLayer(nn.Module):
-    def __init__(self, nfeat, nclass):
-        super(FinalLayer, self).__init__()
-        self.gc = GraphConvolution(nfeat, nclass)
-
-    def forward(self, x, adj):
-        x = self.gc(x, adj)
-        return F.log_softmax(x, dim=1)
+# Example usage:
+# model = GCN(in_channels=16, hidden_channels=32, out_channels=2, num_layers=4)
+# out = model(data.x, data.edge_index)
