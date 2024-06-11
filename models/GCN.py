94a633e766ba6884cf65dfb869b82524a17c1862
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-11 23:38:22 +0900

    Update GCN.py

--- models/GCN.py
+++ models/GCN.py
@@ -1,33 +1,26 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from layers import GraphConvolution
 
 class GCN(nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+    def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
-        assert num_layers >= 2, "Number of layers must be at least 2"
 
-        self.convs = nn.ModuleList()
-        self.bns = nn.ModuleList()
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
 
-        self.convs.append(GCNConv(in_channels, hidden_channels))
-        self.bns.append(nn.BatchNorm1d(hidden_channels))
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
 
-        for _ in range(num_layers - 2):
-            self.convs.append(GCNConv(hidden_channels, hidden_channels))
-            self.bns.append(nn.BatchNorm1d(hidden_channels))
+class FinalLayer(nn.Module):
+    def __init__(self, nfeat, nclass):
+        super(FinalLayer, self).__init__()
+        self.gc = GraphConvolution(nfeat, nclass)
 
-        self.convs.append(GCNConv(hidden_channels, out_channels))
-
-    def forward(self, x, edge_index):
-        for conv, bn in zip(self.convs[:-1], self.bns):
-            x = conv(x, edge_index)
-            x = bn(x)
-            x = F.relu(x, inplace=False)
-        x = self.convs[-1](x, edge_index)  # No ReLU or BatchNorm on the last layer
-        return x
-
-# Example usage:
-# model = GCN(in_channels=16, hidden_channels=32, out_channels=2, num_layers=4)
-# out = model(data.x, data.edge_index)
+    def forward(self, x, adj):
+        x = self.gc(x, adj)
+        return F.log_softmax(x, dim=1)
