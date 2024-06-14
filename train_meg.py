5691529a5f4246b0eb490ea42f8306dfefb66cbc
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-14 12:08:33 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -368,6 +368,7 @@ def train(rank, world_size):
             print(f"Validation loss: {val_loss.item()}")
             print(f"best loss: {best_loss}")
             print(f"Validation accuracy: {val_acc.item() * 100:.2f}%")
+            print(f"best accuracy: {best_acc}")
             print(f"Epoch time: {epoch_time}")
         
             with open(log_file_path, 'a') as f:
