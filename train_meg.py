ebf3ddd83ca3a824750a6b3a1bb6c7a347f0dca9
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-18 00:16:23 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -327,7 +327,7 @@ def train(rank, world_size):
                 v_opt.zero_grad()
                 v_loss = F.mse_loss(value_functions[i], cumulative_rewards[i].unsqueeze(0))
                 # visualize_tensor(v_loss, output_path=f"v_loss_{i}")
-                # print(f"V-network loss for layer {i + 1}: {v_loss.item()}")
+                print(f"V-network loss for layer {i + 1}: {v_loss.item()}")
                 v_loss.backward()
                 torch.nn.utils.clip_grad_norm_(v_network.parameters(), max_norm=0.1)
                 v_opt.step()
