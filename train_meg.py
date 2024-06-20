97818ae8375a1b4953c713b6ed3f87c364250fcf
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-20 11:35:10 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -151,17 +151,17 @@ def train(rank, world_size):
     if rank == 0:
         with open(log_file_path, 'w') as f:
             f.write("Training Log\n")
+            
+    best_loss = load_best_loss()
 
     # 配列を初期化
     epoch_acc_list = []
     val_acc_list = []
     val_loss_list = []
     
-    load_all_weights(adj_generators, gcn_models, v_networks, final_layer)
-    best_loss = load_best_loss()
-    
     # Training loop
     for epoch in range(epochs):
+        load_all_weights(adj_generators, gcn_models, v_networks, final_layer)
         dist.barrier()  # 各エポックの開始時に同期
         start_time = time.time()  # Start the timer at the beginning of the epoch
         epoch_acc = 0
