9e70871ec69671c71e4a49592a409e9aa8694732
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-21 16:16:58 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -219,9 +219,9 @@ def train(rank, world_size):
                         flip_to_1_indices = zeros_indices[torch.randint(len(zeros_indices), (num_flip_to_1,))]
                         new_neighbors[flip_to_1_indices] = 1
 
-                    if num_flip_to_0 > 0:
-                        flip_to_0_indices = ones_indices[torch.randint(len(ones_indices), (num_flip_to_0,))]
-                        new_neighbors[flip_to_0_indices] = 0
+                    # if num_flip_to_0 > 0:
+                    #     flip_to_0_indices = ones_indices[torch.randint(len(ones_indices), (num_flip_to_0,))]
+                    #     new_neighbors[flip_to_0_indices] = 0
                         
                 # ログ確率の計算
                 log_probs = nn.BCEWithLogitsLoss(reduction="sum")(adj_logits + 1e-9, new_neighbors.float())
