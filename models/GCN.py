28d311ba4a40f2c92dbfb9a8b0a60831fb019a87
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-26 12:14:47 +0900

    Update GCN.py

--- models/GCN.py
+++ models/GCN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch_geometric.nn import GCNConv
 
 class GCN(nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout=0.6):
         super(GCN, self).__init__()
 
         # Define a single GCN layer followed by ReLU
