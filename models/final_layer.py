2e7d894df4aa39c646da64344a9195393ec67a03
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-26 12:16:28 +0900

    Update final_layer.py

--- models/final_layer.py
+++ models/final_layer.py
@@ -5,7 +5,7 @@ class FinalLayer(nn.Module):
     def __init__(self, input_dim, num_classes, dropout=0.6):
         super(FinalLayer, self).__init__()
         self.linear = nn.Linear(input_dim, num_classes)
-        self.dropout = nn.Dropout(dropout).to(device)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         x = self.dropout(x)
