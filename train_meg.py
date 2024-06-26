22683770cf1eab3b3b17ed040dc381594c481d34
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-06-26 11:18:15 +0900

    Update train_meg.py

--- train_meg.py
+++ train_meg.py
@@ -201,7 +201,7 @@ def train(rank, world_size):
 
                 adj_logits, new_neighbors = adj_generator.module.generate_new_neighbors(batch.edge_index, updated_features_for_adj)
 
-                print(f"new_adj_probs: {torch.sigmoid(adj_logits / 10)}")
+                print(f"new_adj_probs: {torch.sigmoid(adj_logits / 20)}")
 
                 num_edges = new_neighbors.size(0)
                 if num_edges > 0:
@@ -220,7 +220,7 @@ def train(rank, world_size):
                         new_neighbors[flip_to_0_indices] = 0
                         
                 # ログ確率の計算
-                log_probs = nn.BCEWithLogitsLoss(reduction="sum")(adj_logits / 10 + 1e-9, new_neighbors.float())
+                log_probs = nn.BCEWithLogitsLoss(reduction="sum")(adj_logits / 20 + 1e-9, new_neighbors.float())
                 log_probs_layers.append(log_probs)
                 print(f"log_probs_layers: {log_probs_layers}")
 
