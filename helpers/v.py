b5d329e54b0cdab7feb0a3a9b608710bf2bc40b8
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-07-01 19:14:33 +0900

    Update v.py

--- helpers/v.py
+++ helpers/v.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class VNetwork(nn.Module):
-    def __init__(self, d_model, num_heads, d_ff, num_layers, num_samples, dropout=0.1):
+    def __init__(self, d_model, num_heads, d_ff, num_layers, num_samples):
         super(VNetwork, self).__init__()
         encoder_layer = nn.TransformerEncoderLayer(d_model, num_heads, d_ff, dropout, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers)
