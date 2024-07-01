f3993dbc5be3cada3eb782d6dcda7db3d5fa9bdf
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-07-01 19:18:38 +0900

    Update v.py

--- helpers/v.py
+++ helpers/v.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class VNetwork(nn.Module):
-    def __init__(self, d_model, num_heads, d_ff, num_layers, num_samples):
+    def __init__(self, d_model, num_heads, d_ff, num_layers, num_samples, dropout=0):
         super(VNetwork, self).__init__()
         encoder_layer = nn.TransformerEncoderLayer(d_model, num_heads, d_ff, dropout, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers)
@@ -13,14 +13,13 @@ class VNetwork(nn.Module):
         for layer in self.encoder.layers:
             sa_output, _ = layer.self_attn(x, x, x, need_weights=False)
             sa_output = sa_output.clone()  # Add this line to avoid in-place modification
-            # sa_output = layer.dropout1(sa_output)
+            sa_output = layer.dropout1(sa_output)
             sa_output = layer.norm1(x + sa_output)
 
             ff_output = layer.linear1(sa_output)
-            # ff_output = layer.dropout(layer.activation(ff_output))
-            ff_output = layer.activation(ff_output)
+            ff_output = layer.dropout(layer.activation(ff_output))
             ff_output = layer.linear2(ff_output)
-            x = layer.norm2(sa_output + ff_output)
+            x = layer.norm2(sa_output + layer.dropout2(ff_output))
 
         output = nn.functional.linear(x, self.linear1.weight.clone(), self.linear1.bias).squeeze(2)
         output = nn.functional.linear(output, self.linear2.weight.clone(), self.linear2.bias)
