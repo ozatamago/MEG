b60e6c29c8c659553af9d444cd80bc46362e7fb6
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-07-03 20:14:35 +0900

    Update sampling.py

--- helpers/sampling.py
+++ helpers/sampling.py
@@ -1,7 +1,7 @@
 import torch
 
-def sample_nodes(features, num_of_samples):
-    num_nodes = features.size(0)
-    sample_size = int(num_of_samples)
-    sampled_indices = torch.randperm(num_nodes)[:sample_size]
-    return sampled_indices
+def sample_nodes_from_batch(batch_n_id, num_of_samples):
+    num_nodes = batch_n_id.size(0)  # batch_n_idのノード数を取得
+    sample_size = int(num_of_samples)  # サンプルサイズを整数に変換
+    sampled_indices = torch.randperm(num_nodes)[:sample_size]  # ランダムなインデックスを生成
+    return batch_n_id[sampled_indices]  # ランダムに選択されたノードIDを返す
