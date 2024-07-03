984238387204b3981420ce3aff607893a865a044
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-07-03 20:17:23 +0900

    Update sampling.py

--- helpers/sampling.py
+++ helpers/sampling.py
@@ -1,6 +1,6 @@
 import torch
 
-def sample_nodes_from_batch(batch_n_id, num_of_samples):
+def sample_nodes(batch_n_id, num_of_samples):
     num_nodes = batch_n_id.size(0)  # batch_n_idのノード数を取得
     sample_size = int(num_of_samples)  # サンプルサイズを整数に変換
     sampled_indices = torch.randperm(num_nodes)[:sample_size]  # ランダムなインデックスを生成
