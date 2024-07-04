b637719ef6f3ca018af59c545e1f78442456e0f9
ozatamago <109056512+ozatamago@users.noreply.github.com>
2024-07-04 15:44:28 +0900

    Update pi.py

--- models/pi.py
+++ models/pi.py
@@ -4,25 +4,26 @@ import torch.nn.functional as F
 from torch_geometric.utils import softmax
 
 class MultiHeadGraphAttentionLayer(nn.Module):
-    def __init__(self, in_features, out_features, num_heads, dropout, alpha, concat=True):
+    def __init__(self, in_features, out_features, num_heads, dropout, alpha, concat=True, device='cuda'):
         super(MultiHeadGraphAttentionLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.num_heads = num_heads
         self.alpha = alpha
         self.concat = concat
+        self.device = device
 
         # 各ヘッドに対して重み行列と注意機構を定義
-        self.W = nn.ParameterList([nn.Parameter(torch.empty(size=(in_features, out_features))) for _ in range(num_heads)])
+        self.W = nn.ParameterList([nn.Parameter(torch.empty(size=(in_features, out_features)).to(device)) for _ in range(num_heads)])
         for w in self.W:
             nn.init.xavier_uniform_(w.data, gain=1.414)
 
-        self.a = nn.ParameterList([nn.Parameter(torch.empty(size=(2 * out_features, 1))) for _ in range(num_heads)])
+        self.a = nn.ParameterList([nn.Parameter(torch.empty(size=(2 * out_features, 1)).to(device)) for _ in range(num_heads)])
         for a in self.a:
             nn.init.xavier_uniform_(a.data, gain=1.414)
 
-        self.leakyrelu = nn.LeakyReLU(self.alpha)
-        self.dropout = nn.Dropout(dropout)
+        self.leakyrelu = nn.LeakyReLU(self.alpha).to(device)
+        self.dropout = nn.Dropout(dropout).to(device)
 
     def forward(self, h, adj):
         Wh_all_heads = []
@@ -30,7 +31,7 @@ class MultiHeadGraphAttentionLayer(nn.Module):
             Wh = torch.mm(h, self.W[head])  # 各ヘッドで重み行列を適用
             e = self._prepare_attentional_mechanism_input(Wh, head)
 
-            zero_vec = -9e15 * torch.ones_like(e)
+            zero_vec = -9e15 * torch.ones_like(e).to(self.device)
             attention = torch.where(adj > 0, e, zero_vec)
             attention = self.dropout(attention)
             h_prime = torch.matmul(attention, Wh)
@@ -61,12 +62,12 @@ class AdjacencyGenerator(nn.Module):
 
         # Define Multi-Head Attention Layers
         self.attention_layers = nn.ModuleList([
-            MultiHeadGraphAttentionLayer(d_model, d_model // num_heads, num_heads, dropout, alpha) for _ in range(num_layers)
+            MultiHeadGraphAttentionLayer(d_model, d_model // num_heads, num_heads, dropout, alpha, device=device) for _ in range(num_layers)
         ])
         
         self.dropout = nn.Dropout(dropout).to(device)
         self.weight_vector = nn.Linear(d_model, 1).to(device)
-        self.leaky_relu = nn.LeakyReLU(0.2)  # Add LeakyReLU instance
+        self.leaky_relu = nn.LeakyReLU(0.2).to(device)  # Add LeakyReLU instance
 
     def forward(self, edge_index, x):
         num_nodes = x.size(0)
@@ -115,7 +116,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 x = torch.randn(num_nodes, d_model).to(device)  # ノードの特徴量
 edge_index = torch.randint(0, num_nodes, (2, num_nodes)).to(device)  # エッジのインデックス
 
-adj_generator = AdjacencyGenerator(d_model, num_heads, num_layers, device, dropout, alpha)
+adj_generator = AdjacencyGenerator(d_model, num_heads, num_layers, device, dropout, alpha).to(device)
 adj_logits, new_edges = adj_generator.generate_new_neighbors(edge_index, x)
 print(f"adj_logits: {adj_logits}")
 print(f"new_edges: {new_edges}")
